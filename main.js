662f16375b6eea4503264762d7db89b28faac52a
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-04-24 23:44:41 +0100

    added js

--- main.js
+++ main.js
@@ -10,4 +10,5 @@ console.log(document.head);
 console.log(document.body);
 console.log(document.all);
 console.log(document.all[9]);
-document.all[10].textContent = "Hello";
+// document.all[10].textContent = "Hello";
+console.log(document.forms);
