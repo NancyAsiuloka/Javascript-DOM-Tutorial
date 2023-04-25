40888412b04a1eff0e744bbb4e536688a8d4e26a
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-04-25 23:48:29 +0100

    added comment

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 // Examine the document object
 
 // console.dir(document);
-console.log(document.domain);
-console.log(document.URL);
-console.log(document.title);
-// document.title = 123;
-console.log(document.doctype);
-console.log(document.head);
-console.log(document.body);
-console.log(document.all);
-console.log(document.all[9]);
-// document.all[10].textContent = "Hello";
-console.log(document.forms[0]);
-console.log(document.links);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// // document.title = 123;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[9]);
+// // document.all[10].textContent = "Hello";
+// console.log(document.forms[0]);
+// console.log(document.links);
