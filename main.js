d32e3f9d41afdb40268c90996e3da2cb07c06b85
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-04-26 00:19:15 +0100

    added js

--- main.js
+++ main.js
@@ -27,7 +27,11 @@
 
 // GETELEMENTSBYCLASSNAME
 var items = document.getElementsByClassName('list-group-item');
-
+console.log(items);
+console.log(items[1]);
+items[1].textContent = 'Hello 2';
+items[1].style.fontWeight = 'bold';
+items[1].style.backgroundColor = 'yellow';
 
 
 
