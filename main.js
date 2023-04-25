24965f383fec3fc97c922229e9ee482a4f08b5a1
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-04-26 00:28:48 +0100

    added js

--- main.js
+++ main.js
@@ -26,17 +26,28 @@
 // header.style.borderBottom = 'solid 3px #000';
 
 // GETELEMENTSBYCLASSNAME
-var items = document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[1].textContent = 'Hello 2';
-items[1].style.fontWeight = 'bold';
-items[1].style.backgroundColor = 'yellow';
+// var items = document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent = 'Hello 2';
+// items[1].style.fontWeight = 'bold';
+// items[1].style.backgroundColor = 'yellow';
 
-for(var i = 0; i < items.length; i++){
-    items[i].style.backgroundColor = '#f4f4f4';
-}
+// for(var i = 0; i < items.length; i++){
+//     items[i].style.backgroundColor = '#f4f4f4';
+// }
 
+// GETELEMENTSBYTAGNAME
+// var li = document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[1]);
+// li[1].textContent = 'Hello 2';
+// li[1].style.fontWeight = 'bold';
+// li[1].style.backgroundColor = 'yellow';
+
+// for(var i = 0; i < li.length; i++){
+//     li[i].style.backgroundColor = '#f4f4f4';
+// }
 
 
 
