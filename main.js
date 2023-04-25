3a2fc1aac2ab40f1378e75e7823bb7de141db757
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-04-26 00:42:57 +0100

    added js

--- main.js
+++ main.js
@@ -61,4 +61,17 @@ var submit = document.querySelector('input[type="submit');
 submit.value = 'SEND';
 
 
+var item = document.querySelector('.list-group-item');
+item.style.color = 'red';
+
+var lastItem = document.querySelector('.list-group-item:last-child');
+lastItem.style.color = 'blue';
+
+var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+secondItem.style.color = 'coral';
+
+
+// QUERYSELECTORALL
+
+
 
