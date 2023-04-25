6d9f3e4c9b753229440243fbf1dd6cefbe48074b
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-04-26 00:52:31 +0100

    added js

--- main.js
+++ main.js
@@ -50,28 +50,36 @@
 // }
 
 // QUERYSELECTOR
-var header = document.querySelector('#main-header');
-header.style.borderBottom = 'solid 4px #ccc';
+// var header = document.querySelector('#main-header');
+// header.style.borderBottom = 'solid 4px #ccc';
 
-var input = document.querySelector('input');
-input.value = 'Hello World';
-
-
-var submit = document.querySelector('input[type="submit');
-submit.value = 'SEND';
+// var input = document.querySelector('input');
+// input.value = 'Hello World';
 
 
-var item = document.querySelector('.list-group-item');
-item.style.color = 'red';
+// var submit = document.querySelector('input[type="submit');
+// submit.value = 'SEND';
 
-var lastItem = document.querySelector('.list-group-item:last-child');
-lastItem.style.color = 'blue';
 
-var secondItem = document.querySelector('.list-group-item:nth-child(2)');
-secondItem.style.color = 'coral';
+// var item = document.querySelector('.list-group-item');
+// item.style.color = 'red';
+
+// var lastItem = document.querySelector('.list-group-item:last-child');
+// lastItem.style.color = 'blue';
+
+// var secondItem = document.querySelector('.list-group-item:nth-child(2)');
+// secondItem.style.color = 'coral';
 
 
 // QUERYSELECTORALL
+var titles = document.querySelectorAll('.title');
+console.log(titles);
+titles[0].textContent = 'Hello';
 
+var odd = document.querySelectorAll('li:nth-child(odd)');
+var even = document.querySelectorAll('li:nth-child(even)');
 
-
+for(var i = 0; i < odd.length; i++){
+    odd[i].style.backgroundColor = '#f4f4f4';
+    even[i].style.backgroundColor = '#ccc';
+}
