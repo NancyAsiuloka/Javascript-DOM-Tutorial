4891a40106377f619e238cffcfd459b7c3f9629b
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-04-26 00:22:56 +0100

    added js

--- main.js
+++ main.js
@@ -33,6 +33,13 @@ items[1].textContent = 'Hello 2';
 items[1].style.fontWeight = 'bold';
 items[1].style.backgroundColor = 'yellow';
 
+for(var i = 0; i < items.length; i++){
+    items[i].style.backgroundColor = '#f4f4f4';
+}
+
+
+
+
 
 
 
