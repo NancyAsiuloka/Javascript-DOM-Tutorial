d2ec44496de9766f2f129ea7fea9ca444fb57303
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-04-27 00:53:37 +0100

    added js

--- main.js
+++ main.js
@@ -72,14 +72,17 @@
 
 
 // QUERYSELECTORALL
-var titles = document.querySelectorAll('.title');
-console.log(titles);
-titles[0].textContent = 'Hello';
+// var titles = document.querySelectorAll('.title');
+// console.log(titles);
+// titles[0].textContent = 'Hello';
 
-var odd = document.querySelectorAll('li:nth-child(odd)');
-var even = document.querySelectorAll('li:nth-child(even)');
+// var odd = document.querySelectorAll('li:nth-child(odd)');
+// var even = document.querySelectorAll('li:nth-child(even)');
 
-for(var i = 0; i < odd.length; i++){
-    odd[i].style.backgroundColor = '#f4f4f4';
-    even[i].style.backgroundColor = '#ccc';
-}
+// for(var i = 0; i < odd.length; i++){
+//     odd[i].style.backgroundColor = '#f4f4f4';
+//     even[i].style.backgroundColor = '#ccc';
+// }
+
+// TRAVERSING THE DOM
+
