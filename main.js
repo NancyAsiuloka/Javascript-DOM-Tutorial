8e5cc8c26582bf59a25689329e627bebfdf81829
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-05-07 15:19:43 +0100

    added js

--- main.js
+++ main.js
@@ -208,7 +208,7 @@ function runEvent(e){
     console.log('EVENT TYPE: '+e.type)
     output.innerHTML = '<h3>MouseX:'+e.offsetX+' </h3><h3>MouseY:'+e.offsetY+'</h3>';
 
-     document.body.style.backgroundColor = 'rgb('+e.offsetX+','+e.offsetY+', 40)';
+     box.style.backgroundColor = 'rgb('+e.offsetX+','+e.offsetY+', 40)';
 }
 
 
