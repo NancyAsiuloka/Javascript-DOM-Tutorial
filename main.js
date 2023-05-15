fa0d7ef7a7d48436b91fc8ad2d42eb2ab6b0a9e7
Nancy Asiuloka <nancyasiuloka@gmail.com>
2023-05-15 11:10:23 +0100

    added js

--- main.js
+++ main.js
@@ -245,13 +245,15 @@ var itemList = document.getElementById('items');
 
 // Form submit event
 form.addEventListener('submit', addItem);
+// Delete event
+itemList.addEventListener('click', removeItem);
 
 // Add item
 function addItem(e){
     e.preventDefault();
 
     // Get input value
-    var newItem = document.getElementById('item');
+    var newItem = document.getElementById('item').value;
 
     // Create new li element
     var li = document.createElement('li');
@@ -260,10 +262,22 @@ function addItem(e){
     // Add text node with input value
     li.appendChild(document.createTextNode(newItem));
 
+    // Create the delete button element
+    var deleteBtn = document.createElement('button');
+
+    // Add classes to del button
+    deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+    // Append text node
+    deleteBtn.appendChild(document.createTextNode('X'));
+
+    // Append button to li
+    li.appendChild(deleteBtn);
+
+    // Append li to list
     itemList.appendChild(li);
 }
 
-
+// Remove items
 
 
 
